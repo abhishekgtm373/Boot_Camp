b8299198e2bd597750ca7f977a58278dc8715c01
abhishekgtm373 <abhishekgtm373@gmail.com>
2021-03-11 22:13:29 +0530

    Create Finding_Vowels.py

---
+++ Finding_Vowels.py
@@ -0,0 +1,34 @@
+#finding vowels in user entered string
+ch=input("Enter the desired sentence or word\n")
+ch=list(ch)
+sizeoflist=len(ch)
+found=[]
+logic_holder=[0,0,0,0,0]
+vowels=['a','e','i','o','u']
+
+for i in ch:
+    if i in vowels:
+        if i not in found:
+            found.append(i)
+
+
+
+i=0
+for i in range (sizeoflist):
+    if ch[i]=='a' or ch[i]=='A':
+        logic_holder[0]=int(logic_holder[0])+1
+    elif ch[i]=='e' or ch[i]=='E':
+        logic_holder[1]=int(logic_holder[1])+1
+    elif ch[i] =='i' or ch[i]=='I':
+        logic_holder[2]=int(logic_holder[2])+1
+    elif ch[i] =='o' or ch[i]=='O':
+        logic_holder[3]=int(logic_holder[3])+1
+    elif ch[i] =='u' or ch[i]=='U':
+        logic_holder[4]=int(logic_holder[4])+1
+
+for i in range(4,-1,-1):
+    if logic_holder[i]==0:
+        del logic_holder[i]
+
+found.sort()
+print('vowels in sentence—',found,'\n','vowel repeated as—',logic_holder)
